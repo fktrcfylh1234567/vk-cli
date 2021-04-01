21274b0ed044d7ee0115771bb81c3d513875b9e7
Aleksander Vorobiev <avorobiev@cybertonica.com>
2021-04-01 19:42:58 +0300

    add users api

---
+++ api/users.js
@@ -0,0 +1,16 @@
+async function getUserFullName(vk, userId) {
+    const info = await getUserInfo(vk, userId)
+    return info.first_name + " " + info.last_name
+}
+
+/**
+ * @return {{first_name:string, last_name:string}}
+ */
+async function getUserInfo(vk, userId) {
+    return await vk.call('users.get', {
+        user_ids: userId,
+    })
+}
+
+exports.getUserInfo = getUserInfo;
+exports.getUserFullName = getUserFullName;
