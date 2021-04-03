58ce28992a2be7c6d9d02b61ed234aea50e4f233
Aleksander Vorobiev <avorobiev@cybertonica.com>
2021-04-03 14:35:38 +0300

    move enrichment for longpolls

--- api/longpoll.js
+++ api/longpoll.js
@@ -1,9 +1,4 @@
-/**
- * Called after doing asynchronous stuff.
- * @name OnNewMessageCallback
- * @function
- * @param {String} msg
- */
+const vkApi = require("./vk");
 
 /**
  * @param {{longpoll:Object}} vk
@@ -11,8 +6,8 @@
  */
 function listenForMessages(vk, callback) {
     vk.longpoll.connect().then((connection) => {
-        connection.on('message', (it) => {
-            const messageParsed = parseMessage(it)
+        connection.on('message', async it => {
+            const messageParsed = await parseMessage(it)
             callback(messageParsed)
         })
     })
@@ -25,15 +20,18 @@ exports.listenForMessages = listenForMessages;
  * @param {Array} msgRaw
  * @return {{text: string, peer_id: number}}
  */
-function parseMessage(msgRaw = []) {
-    const PEER = 3;
+async function parseMessage(msgRaw = []) {
+    const PEER_ID = 3;
     const TEXT = 5;
     const INFO = 6;
 
-    const msg = {
+
+    let msg = {
         text: msgRaw[TEXT],
-        peer_id: msgRaw[PEER]
+        peer_id: msgRaw[PEER_ID]
     }
+
+    msg = await enrichMessage(msg)
 
     const info = msgRaw[INFO]
 
@@ -43,6 +41,25 @@ function parseMessage(msgRaw = []) {
 
     if (info.hasOwnProperty("attach1_type")) {
         msg.attachment_type = info.attach1_type
+    }
+
+    return msg
+}
+
+async function enrichMessage(msg) {
+    const vk = await vkApi.getVK();
+
+    const res = await vk.call('messages.getConversationsById', {
+        peer_ids: msg.peer_id,
+        extended: true
+    })
+
+    if (res.profiles.length === 1) {
+        msg.title = res.profiles[0].first_name + ' ' + res.profiles[0].last_name
+    }
+
+    if (res.items[0].hasOwnProperty("chat_settings")) {
+        msg.title = res.items[0].chat_settings.title
     }
 
     return msg
